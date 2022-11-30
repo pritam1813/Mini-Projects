874bd3fd399d517e8e8c406f5bf2ae77abc68cdb
pritam1813 <pritam1813@gmail.com>
2022-11-30 19:03:23 +0530

    Some try with Pug

--- Nodejs/Express/TodoListApp/index.js
+++ Nodejs/Express/TodoListApp/index.js
@@ -1,6 +1,19 @@
-const { application } = require('express');
 const express = require('express');
+const app = express();
 const port = 3000;
 
+//Setting View Engine/ Template Engine
+app.set('view engine','pug');
+app.set('views','./views');
 
+app.get('/', function(req, res){
+    res.render('home',{title : 'Pug Template'});
+});
+
+app.listen(port,function(err){
+    if(err){
+        console.log("error");
+    }    
+    console.log("No error");
+});
 
